cc755fca36e55ee71dfcb326b9b18daf87282c20
Jonathan Huo <jonhuo11@gmail.com>
2024-11-26 22:40:44 -0500

    wip, adding load balancing

---
+++ src/lb_udp.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cstring>
+#include <memory>
+#include <random>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+// services that the load balancer is distributing traffic across
+class ServiceSocketUDP {
+    int socket;
+    struct sockaddr_in destAddr;
+
+   public:
+    ServiceSocketUDP(const char* ip, uint16_t port);
+    ~ServiceSocketUDP();
+    int send(const char* message);
+};
+
+// socket that server reads incoming udp traffic from
+class ServerSocketUDP {
+};
+
+struct ClientIdentifier {
+    uint32_t ip;    // IP address in binary form
+    uint16_t port;  // Port in binary form
+
+    bool operator==(const ClientIdentifier& other) const {
+        return ip == other.ip && port == other.port;
+    };
+};
+
+template <>
+struct hash<ClientIdentifier> {
+    size_t operator()(const ClientIdentifier& client) const {
+        return std::hash<uint32_t>()(client.ip) ^ std::hash<uint16_t>()(client.port);
+    }
+};
+
+// incoming packet read from server socket
+struct Packet {
+    ClientIdentifier sender;
+
+    // TODO: what goes here?
+};
+
+// load balancer
+class LoadBalancerUDP {
+    const vector<unique_ptr<ServiceSocketUDP>>& services;
+
+   public:
+    LoadBalancerUDP(const vector<unique_ptr<ServiceSocketUDP>>& services, unsigned int balanceStrategy);
+
+   private:
+    class BalanceStrategy {
+       protected:
+        LoadBalancerUDP& lb;
+
+       public:
+        BalanceStrategy(LoadBalancerUDP& lb) : lb(lb) {};
+        virtual size_t calculateDestinationServiceForPacket(Packet& p) = 0;  // returns the index of the service socket to send packet to
+        virtual ~BalanceStrategy() = default;
+    };
+
+    // randomly pick a service and send the packet to it
+    class RandomBalance : public BalanceStrategy {
+        random_device rdev;
+        mt19937 gen;
+        uniform_int_distribution<> distr;
+
+       public:
+        RandomBalance(LoadBalancerUDP& lb);
+        size_t calculateDestinationServiceForPacket(Packet& p) override;
+    };
+
+    // keep track of which clients are mapped to which services
+    class RoundRobinServiceAssignmentBalance : public BalanceStrategy {
+        size_t round_i = 0;  // which is the next server to assign to
+        unordered_map<ClientIdentifier, size_t> clientServiceMap;
+
+       public:
+        size_t calculateDestinationServiceForPacket(Packet& p) override;
+    };
+
+    unique_ptr<BalanceStrategy> balanceStrategy;
+};
--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,15 @@
-#include <iostream>
-#include <fstream>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <cstring>
 #include <sys/epoll.h>
-#include <vector>
-#include <memory>
+#include <sys/socket.h>
+#include <unistd.h>
 
-#include "sockets.cc"
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include "lb_udp.h"
 
 #define MAX_EVENTS 10
 #define BUFFER_SIZE 1024
@@ -16,149 +17,146 @@
 using namespace std;
 
 struct Config {
-    int listen_port;
+    int listenPort;
     int mode;
-    int client_count;
-    uint16_t* client_ports;
-    Config(): listen_port(0), mode(0), client_count(0), client_ports(nullptr) {}
+    int serviceCount;
+    uint16_t *servicePorts;
+    Config() : listenPort(0), mode(0), serviceCount(0), servicePorts(nullptr) {}
     ~Config() {
-        delete[] client_ports;
+        delete[] servicePorts;
     }
 };
 
-ostream& operator<<(std::ostream& out, const Config& val){
-    out << "Listen port: " << val.listen_port << "\nMode: " << val.mode << "\nClients: " << val.client_count << "\n";
+ostream &operator<<(std::ostream &out, const Config &val) {
+    out << "Listen port: " << val.listenPort << "\nMode: " << val.mode << "\nClients: " << val.serviceCount << "\n";
     return out;
 }
 
-
-int main(int argc, char* argv[]) {
-
+int main(int argc, char *argv[]) {
     // get destination ports from config.txt
     if (argc != 2) {
         cerr << "Usage: openloadbalancer [config file]" << endl;
         return 1;
     }
 
-    ifstream config_file;
-    config_file.open(argv[1]);
-    if (!config_file) {
+    ifstream configFile;
+    configFile.open(argv[1]);
+    if (!configFile) {
         cerr << "Could not find config file " << argv[1] << endl;
         return 1;
     }
     Config config;
 
-    if (!(config_file >> config.listen_port)) {
+    if (!(configFile >> config.listenPort)) {
         cerr << "Could not read listening port" << endl;
         return 1;
     }
 
     // 0 is round robin
     // 1 is IP hashing
-    if (!(config_file >> config.mode)) {
+    if (!(configFile >> config.mode)) {
         cerr << "Could not read load balance mode" << endl;
         return 1;
     }
 
-    if (!(config_file >> config.client_count)) {
+    if (!(configFile >> config.serviceCount)) {
         cerr << "Could not read client count" << endl;
         return 1;
     }
 
-    config.client_ports = new uint16_t[config.client_count];
-    for (int i = 0; i < config.client_count; i++) {
-        int client_port;
-        if (!(config_file >> client_port)) {
+    config.servicePorts = new uint16_t[config.serviceCount];
+    for (int i = 0; i < config.serviceCount; i++) {
+        int clientPort;
+        if (!(configFile >> clientPort)) {
             cerr << "Could not read client " << i << "'s port number" << endl;
             return 1;
         }
-        config.client_ports[i] = client_port;
+        config.servicePorts[i] = clientPort;
     }
 
-    config_file.close();
+    configFile.close();
 
     cout << config << endl;
 
     // open client sockets for writing
-    vector<unique_ptr<ClientSocketUDP>> clients(config.client_count);    
-    for (int i = 0; i < config.client_count; i++) {
-        clients[i] = make_unique<ClientSocketUDP>("127.0.0.1", config.client_ports[i]);
-        cout << "Opened client socket at " << config.client_ports[i] << endl;
+    vector<unique_ptr<ServiceSocketUDP>> services(config.serviceCount);
+    for (int i = 0; i < config.serviceCount; i++) {
+        services[i] = make_unique<ServiceSocketUDP>("127.0.0.1", config.servicePorts[i]);
+        cout << "Opened client socket at " << config.servicePorts[i] << endl;
     }
 
-
     // listen on socket for events
-    int udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (udp_socket < 0) {
+    int udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udpSocket < 0) {
         cerr << "Error creating socket" << endl;
         return 1;
     }
 
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;                       // IPv4
-    server_addr.sin_port = htons(config.listen_port);                     // Port number
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");   // Localhost IP address
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;                      // IPv4
+    serverAddr.sin_port = htons(config.listenPort);       // Port number
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");  // Localhost IP address
 
-    if (bind(udp_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(udpSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
         cerr << "Error binding socket" << endl;
-        close(udp_socket);
+        close(udpSocket);
         return 1;
     }
 
     cout << "\nLoad balancer started" << endl;
 
-    int epoll_fd = epoll_create1(0);
-    if (epoll_fd == -1) {
+    int epollFd = epoll_create1(0);
+    if (epollFd == -1) {
         cerr << "Error creating epoll" << endl;
-        close(udp_socket);
+        close(udpSocket);
         return 1;
     }
 
     struct epoll_event event, events[MAX_EVENTS];
-    struct sockaddr_in client_addr;
-    socklen_t client_len = sizeof(client_addr);
+    struct sockaddr_in clientAddr;
+    socklen_t clientLen = sizeof(clientAddr);
     char buffer[BUFFER_SIZE];
 
     event.events = EPOLLIN;
-    event.data.fd = udp_socket;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, udp_socket, &event) == -1) {
+    event.data.fd = udpSocket;
+    if (epoll_ctl(epollFd, EPOLL_CTL_ADD, udpSocket, &event) == -1) {
         cerr << "epoll_ctl: udp_socket" << endl;
-        close(udp_socket);
-        close(epoll_fd);
+        close(udpSocket);
+        close(epollFd);
         return 1;
     }
 
-    int send_client = 0;
+    int sendClient = 0;
     for (;;) {
-        int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1); // nubmer of events
+        int nfds = epoll_wait(epollFd, events, MAX_EVENTS, -1);  // number of events
         if (nfds == -1) {
             cerr << "epoll_wait failed" << endl;
-            close(udp_socket);
-            close(epoll_fd);
+            close(udpSocket);
+            close(epollFd);
             return 1;
         }
 
         // does not execute if nfds < 1
         for (int i = 0; i < nfds; ++i) {
-            if (events[i].events & EPOLLIN) { // Data is available to read
+            if (events[i].events && EPOLLIN) {  // Data is available to read
                 memset(buffer, 0, BUFFER_SIZE);
-                ssize_t n = recvfrom(udp_socket, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_len);
+                ssize_t n = recvfrom(udpSocket, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&clientAddr, &clientLen);
                 if (n < 0) {
                     cerr << "recv failed" << endl;
                 } else {
-
                     // TODO: migrate into lb.cc
-                    if (send_client >= clients.size()) { send_client = 0; } // simple round robin schedule
-                    clients[send_client]->send(buffer);
-                    send_client++;
+                    if (sendClient >= services.size()) {
+                        sendClient = 0;
+                    }  // simple round robin schedule
+                    services[sendClient]->send(buffer);
+                    sendClient++;
                 }
             }
         }
     }
 
-
-    close(udp_socket);
-    close(epoll_fd);
+    close(udpSocket);
+    close(epollFd);
     return 0;
 }
